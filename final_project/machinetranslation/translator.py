9f5c71f5b5a9eb4831060dc5b6319af5692b0b52
Steellow <n0kiha00@students.oamk.fi>
2022-12-18 08:37:16 -0500

    Final changes

---
+++ final_project/machinetranslation/translator.py
@@ -0,0 +1,41 @@
+import json
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+apikey = os.environ['apikey']
+url = os.environ['url']
+
+
+def translator_instance():
+    authenticator = IAMAuthenticator(apikey)
+    language_translator = LanguageTranslatorV3(
+        version='2018-05-01',
+        authenticator=authenticator
+    )
+    language_translator.set_service_url(url)
+
+    return language_translator
+
+def english_to_french(english_text):
+    if not english_text:
+        return ""
+    translator = translator_instance()
+    french_text = translator.translate(
+        text=english_text,
+        model_id="en-fr").get_result()["translations"][0]["translation"]
+
+    return french_text
+
+def french_to_english(french_text):
+    if not french_text:
+        return ""
+    translator = translator_instance()
+    english_text = translator.translate(
+        text=french_text,
+        model_id="fr-en").get_result()["translations"][0]["translation"]
+
+    return english_text
